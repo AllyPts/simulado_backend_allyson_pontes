e73de8fa65df75d897e5f0f2e18a2c9e4189e263
unknown <Administrador@UNI001-L008-001.serlab.intra>
2026-03-27 10:41:42 -0300

    alteração_allyson_pontes

--- calculadora/calculator.py
+++ calculadora/calculator.py
@@ -4,13 +4,14 @@ class Calculator:
         return a + b
 
     def subtract(self, a: float, b: float) -> float:
-        # TODO(aluno): implementar subtracao.
-        raise NotImplementedError("Metodo subtract ainda nao foi implementado.")
+        """Metodo subitração implementado"""
+        return a - b 
 
     def multiply(self, a: float, b: float) -> float:
-        # TODO(aluno): implementar multiplicacao.
-        raise NotImplementedError("Metodo multiply ainda nao foi implementado.")
-
+        """Metodo multiplicação implementado"""
+        return a * b
     def divide(self, a: float, b: float) -> float:
-        # TODO(aluno): implementar divisao e tratar divisao por zero.
-        raise NotImplementedError("Metodo divide ainda nao foi implementado.")
+        """Metodo divisão implementado"""
+        if b == 0:
+            raise ValueError("divisão por zero não é permitida.")
+        return a / b
--- calculadora/tests.py
+++ calculadora/tests.py
@@ -8,4 +8,20 @@ class CalculatorTestCase(TestCase):
         calc = Calculator()
         self.assertEqual(calc.add(2, 3), 5)
 
-    # TODO(aluno): adicionar testes para subtract, multiply e divide.
+    def test_subtract(self):
+
+        calc = Calculator()
+        self.assertEqual(calc.subtract(5, 3), 2)
+
+    def test_multiply(self):
+        calc = Calculator()
+        self.assertEqual (calc.multiply(4, 3), 12)
+    
+    def test_divide(self):
+        calc = Calculator()
+        self.assertEqual(calc.divide(10, 2), 5)
+
+    def test_divide_by_zero(self):
+        calc = Calculator()
+        with self,assertRaises(ValueError):
+            calc.divide(10, 0)
